32fee9ec997d35bed5ada1479ecd2078079f2dfb
jolozy <joannelozy@outlook.com>
2016-03-28 12:17:23 +0800

    this is a conflict

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,26 @@
 class MoviesController < ApplicationController
 
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def actor_params
+    params.require(:movie).permit(:title, :summary, :youtube_embedded_id, :thumbnail)
+  end
+
 end
